81cbd93959aea13a61d9b082a6fee02d097824e4
doyochi <nuzululhikmah117@gmail.com>
2022-06-03 10:27:30 +0700

    more

--- app/src/main/java/id/hikmah/binar/jetpack/MainActivity.kt
+++ app/src/main/java/id/hikmah/binar/jetpack/MainActivity.kt
@@ -3,30 +3,93 @@ package id.hikmah.binar.jetpack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         setContent {
             DecoratedComposablePreview()
         }
     }
-
+    
     @Composable
     fun SimpleComposable(){
-        Text ("Hello World")
+        Column() {
+            Text(
+                "Hello World",
+                fontSize = 12.sp,
+                color = Color.Gray,
+                style = TextStyle(),
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold
+            )
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Ini Button")
+            }
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "Ini text button")
+            }
+            TextField(value = "ini text fiels", onValueChange = {})
+        }
+        Text(
+            "Hello World",
+            fontSize = 12.sp,
+            color = Color.Gray,
+            style = TextStyle(),
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold
+        )
+
+////        Text("Hello World", fontSize = TextUnit.Unspecified, color = Color.Gray)
+//        Button(onClick = { /*TODO*/}) {
+//
+//        }
+//        TextButton(onClick = { /*TODO*/ }) {
+//
+//        }
+//        TextField(value = "", onValueChange = {})
+//        Column() {
+//
+//        }
+//        Row() {
+//
+//        }
     }
 
-//    @Preview(shadowSystemUi = true)
+    @Composable
+    fun StyledTextField() {
+        var value by remember { mutableStateOf("Hello\nWorld\nInvisible") }
+
+        TextField(
+            value = value,
+            onValueChange = { value = it },
+            label = { Text("Enter text") }
+        )
+    }
+
+    @Preview(showSystemUi = false)
     @Composable
     fun DecoratedComposablePreview(){
         Text("Hello World")
     }
 
+    @Preview(showSystemUi = false)
     @Composable
     fun ComposablePreview() {
         SimpleComposable()
--- app/src/main/java/id/hikmah/binar/jetpack/Page2Activity.kt
+++ app/src/main/java/id/hikmah/binar/jetpack/Page2Activity.kt
@@ -3,8 +3,11 @@ package id.hikmah.binar.jetpack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -13,7 +16,20 @@ class Page2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_page2)
         setContent {
-            TitleText(title = "Hello World")
+            Column {
+                TitleText(title = "Hello World")
+            }
+        }
+    }
+
+    @Preview(showSystemUi = false)
+    @Composable
+    fun BodyPage2(){
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+//            SetTextStyling(displayText = "Hello")
         }
     }
 
